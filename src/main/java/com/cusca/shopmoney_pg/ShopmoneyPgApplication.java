fec86db04f513a3d54546bb950909ea4eb773da7
mrxSteve <steventobar38@gmail.com>
2025-10-15 16:51:51 -0600

    feat: configuracion inicial del proyecto

---
+++ src/main/java/com/cusca/shopmoney_pg/ShopmoneyPgApplication.java
@@ -0,0 +1,13 @@
+package com.cusca.shopmoney_pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmoneyPgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmoneyPgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cusca/shopmoney_pg/ShopmoneyPgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cusca.shopmoney_pg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmoneyPgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
